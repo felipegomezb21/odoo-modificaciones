d345d0e617a27f483b5b8ad00644276c44b3920f
felipegomezb21 <felipegomezb21@gmail.com>
2023-02-08 21:25:34 -0500

    First commit

---
+++  account_print_receipt/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Luis Felipe Gomez Botero
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Receipt Print",
+    "version": "14.0.1",
+    "category": "Accounting & Finance",
+    "summary": "Enable printing thermal receipt in invoice",
+    "author": "Luis Felipe Gomez Botero",
+    "website": "https://github.com/felipegomezb21/odoo-modificaciones.git",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/report_receipt.xml",
+        "views/account_report.xml",
+    ],
+    "installable": True,
+}
