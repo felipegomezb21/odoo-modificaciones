5a8d0e6e4435a9c9fd3ff8e623b6db4cabe288ac
felipegomezb21 <felipegomezb21@gmail.com>
2023-10-02 20:40:12 -0500

    Add sales management custom restriction

---
+++ sales_management_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Luis Felipe Gomez Botero
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sales Management Custom",
+    "version": "14.0.1",
+    "category": "Accounting & Finance",
+    "summary": "Restrict diferent sales fields",
+    "author": "Luis Felipe Gomez Botero",
+    "website": "https://github.com/felipegomezb21/odoo-modificaciones.git",
+    "license": "AGPL-3",
+    "depends": ["sale_management"],
+    "data": [
+        "views/sale.view_order_form_views"
+    ],
+    "installable": True,
+}
