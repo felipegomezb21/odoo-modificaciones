77ca5f85c3045f7e921570541728746d797a1dc0
felipegomezb21 <felipegomezb21@gmail.com>
2024-09-16 19:59:46 -0500

    add custom view for stock module with filter responsable

---
+++ account_custom copy/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Luis Felipe Gomez Botero
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Custom",
+    "version": "14.0.1",
+    "category": "Stock",
+    "summary": "Views Custom",
+    "author": "Luis Felipe Gomez Botero",
+    "website": "https://github.com/felipegomezb21/odoo-modificaciones.git",
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "data": [
+        "views/stock.picking.form.custom.xml"
+    ],
+    "installable": True,
+}
