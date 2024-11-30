8cdc922d3fc109a3681c01e2c8f114191bdc491e
felipegomezb21 <felipegomezb21@gmail.com>
2024-11-30 12:08:40 -0500

    add dashboard modules

---
+++ dashboard_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Luis Felipe Gomez Botero
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Dashboard Custom",
+    "version": "14.0.1.0.0",
+    "category": "Tools",
+    "summary": "Custom Dashboard View",
+    "author": "Luis Felipe Gomez Botero",
+    "website": "https://github.com/felipegomezb21/odoo-modificaciones.git",
+    "license": "AGPL-3",
+    "depends": ["base", "web"],
+    "data": [
+        "views/board.sales.custom.xml",
+    ],
+    "installable": True,
+}
