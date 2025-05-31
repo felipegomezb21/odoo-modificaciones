65000b90ded24dcfa059f073e7c41ee156170e35
felipegomezb21 <felipegomezb21@gmail.com>
2025-05-31 09:13:06 -0500

    Create stock delivery receipt module

---
+++ stock_print_receipt/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Luis Felipe Gomez Botero
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Delivery Receipt Print",
+    "version": "14.0.1",
+    "category": "Stock",
+    "summary": "Enable printing thermal receipt in delivery receipt",
+    "author": "Luis Felipe Gomez Botero",
+    "website": "https://github.com/felipegomezb21/odoo-modificaciones.git",
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "data": [
+        #"views/report_invoice_with_payments_custom_receipt.xml",
+        "views/report_deliveryslip_receipt.xml"
+        #"views/external_layout_receipt.xml",
+        #"views/external_layout_clean_receipt.xml",
+        #"views/address_layout_receipt.xml"
+    ],
+    "installable": True,
+}
